4d955e348966baa3a4fd0d793d1bed2a870f107b
doguncle <13664237263@139.com>
2024-06-06 23:06:56 +0800

    文件结构建立项目结构

---
+++ Model/UserModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFToDoList.Model
+{
+    public class UserModel
+    {
+        public string UserID;
+        public string UserName;
+        public string Password;
+        public string Email;
+        public string PhoneNo;
+    }
+}

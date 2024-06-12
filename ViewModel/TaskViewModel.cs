6cdef295f5666793bd070ad58b2ed2345f643e30
doguncle <13664237263@139.com>
2024-06-12 23:03:28 +0800

    1.0.0 task modules develop

--- ViewModel/TaskViewModel.cs
+++ ViewModel/TaskViewModel.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Prism.Mvvm;
+using Prism.Regions;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,14 +14,46 @@ namespace WPFToDoList.ViewModel
     /// <summary>
     /// ViewModel层
     /// </summary>
-    public class TaskViewModel
+    public class TaskViewModel : BindableBase, INavigationAware
     {
-        public TaskService taskService=new TaskService();
+        public TaskService taskService = new TaskService();
 
         public List<TaskModel> GetTaskData()
         {
             return taskService.GetTaskData(Data.OptionType.offlineType);
         }
 
+        //需要继承INavigationAware接口，实现接口中三个方法
+        //三个方法的执行顺序：
+        //1.Main====>PageA
+        //先执行Main页面的OnNavigatedFrom，再执行PageA页面的OnNavigatedTo
+        //2.PageA====>PageA
+        //OnNavigatedFrom()----->IsNavigationTarget()----->OnNavigatedTo()
+        public bool IsNavigationTarget(NavigationContext navigationContext)
+        {
+            //每次重新导航时，是否重新创建当前PageBViewModel实例，
+            //return true;时就不会再创建新的实例（不会再次运行当前PageBViewModel构造函数）
+            return true;
+        }
+
+        /// <summary>
+        /// // 从这个ViewModel导航离开时的逻辑
+        /// </summary>
+        /// <param name="navigationContext"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        public void OnNavigatedFrom(NavigationContext navigationContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// // 导航到此ViewModel时的逻辑
+        /// </summary>
+        /// <param name="navigationContext"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        public void OnNavigatedTo(NavigationContext navigationContext)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
